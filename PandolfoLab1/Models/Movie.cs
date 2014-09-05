4a3961981209043258cbdc2a4ea715675293c44d
1234901 <nic_pandolfo@hotmail.com>
2014-09-05 11:47:06 -0400

    Final moins page 7 et partie de 5

--- PandolfoLab1/Models/Movie.cs
+++ PandolfoLab1/Models/Movie.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace PandolfoLab1.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+        
+        [Required]
         public string Title { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        
+        [Required]
         public string Genre { get; set; }
+        
+        [Range(1,100)]
+        public decimal Price { get; set; }
+
+        [StringLength(5)]
+        public string Rating { get; set; }
+       
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
     }
 
